a6b9c222681ba556eb263ab8b3444df5b9168e91
duandjs <569181876>
2020-01-17 09:06:23 +0800

    03

--- src/main/java/com/tenstest/base/controller/LableController.java
+++ src/main/java/com/tenstest/base/controller/LableController.java
@@ -1,17 +1,11 @@
 package com.tenstest.base.controller;
 
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin
-@RequestMapping("/label")
 public class LableController {
-
-    @RequestMapping(method = RequestMethod.GET)
+    @GetMapping("/")
     public Result findAll(){
         return new Result(true, StatusCode.OK,"查询成功","hello world");
     }
